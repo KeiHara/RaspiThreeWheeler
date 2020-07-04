b7d75d0aec9566649f9edd1a8bad8b294504a239
KieHr <50412162+KeiHara@users.noreply.github.com>
2020-07-04 15:30:35 +0900

    Update motor.py
    simple motor

--- motor.py
+++ motor.py
@@ -3,39 +3,35 @@ import time
 import curses
 
 class Motor:
-	def __init__(self):
-		GPIO.cleanp()
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(16, GPIO.OUT)
-		GPIO.setup(18, GPIO.OUT)
+    def __init__(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        self.fpin = 16 # forward
+        self.bpin = 18 # backword
+        GPIO.setup(self.fpin, GPIO.OUT)
+        GPIO.setup(self.bpin, GPIO.OUT)
+        self.forward = GPIO.PWM(self.fpin, 50)
+        self.backward = GPIO.PWM(self.bpin, 50)
+        self.forward.start(0)
+        self.backward.start(0)
+    
+    def MotorDrive(self):
+        count = 0
+        while True:
+            if count == 10:
+                self.forward.stop()
+                self.backward.stop()
+                break
+            else:
+                self.forward.ChangeDutyCycle(50)
+                self.backward.ChangeDutyCycle(0)
+                time.sleep(3)
+                self.forward.ChangeDutyCycle(0)
+                self.backward.ChangeDutyCycle(50)
+                time.sleep(3)
+                count += 1
 
-def MotorDrive( iIn1Pin, iIn2Pin, motor ):
-	if 5 > motor and -5 < motor:
-		GPIO.pwmWrite( iIn1Pin, 0.0 )
-		GPIO.pwmWrite( iIn2Pin, 0.0 )
-	elif 0 < motor:
-		GPIO.pwmWrite( iIn1Pin, motor * 0.01 )
-		GPIO.pwmWrite( iIn2Pin, 0.0 )
-	else:
-		GPIO.pwmWrite( iIn1Pin, 0.0 )
-		GPIO.pwmWrite( iIn2Pin, -motor * 0.01 )
-
-IN1PIN = 23
-IN2PIN = 24
-
-GPIO.setFunction( IN1PIN, GPIO.PWM )
-GPIO.setFunction( IN2PIN, GPIO.PWM )
-
-motor = 0
-increase = 10
-
-while 1:
-	MotorDrive( IN1PIN, IN2PIN, motor )
-	motor += increase
-	if 100 < motor:
-		motor = 90
-		increase = -10
-	if -100 > motor:
-		motor = -90
-		increase = 10
-	webiopi.sleep( 1.0 )
+	
+motor = Motor()
+motor.MotorDrive()
+GPIO.cleanup()
